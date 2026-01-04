f192c3461056ac1b44c76b3fd4cdc246bbf06ac7
Louis Gerardo <louisgerardoserranoalamo@gmail.com>
2026-01-04 14:28:31 -0600

    feat: set up character routes and initialize database schema in index file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express, {Request, Response} from 'express';
 import dotenv from 'dotenv';
+import characterRouter from './router/character.router';
+import { initSchema } from './database/schema';
 
 dotenv.config();
+initSchema();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// Rutas
+app.use('/api/characters', characterRouter);
 
 app.get('/', (req: Request, res: Response) => {
     res.send(`Server is running on port ${PORT}`);
